3013eb5f5300add18b821e90603fef65739de6d2
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-31 23:24:08 -0400

    Create trial_upsell.js

---
+++ trial_upsell.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(a){a(document).on("click",'[data-action="upsell-freetrial"]',function(f){var d=a(this),c=a(this).data("hook"),b=false;f.preventDefault();switch(c){case"unlock":b="#trial-unlock-overlay";break;case"expired":b="#trial-expired-overlay";break;case"biz-content":b="#trial-biz-content-overlay";break}if(b&&a(b).length>0){a(b).modal()}})})(jQuery);
+
+}
+/*
+     FILE ARCHIVED ON 16:17:30 Apr 15, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:23:52 Jun 01, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.35
+  exclusion.robots.policy: 0.323
+  RedisCDXSource: 11.033
+  PetaboxLoader3.datanode: 281.02 (4)
+  esindex: 0.026
+  captures_list: 111.777
+  LoadShardBlock: 72.403 (3)
+  PetaboxLoader3.resolve: 181.929
+  CDXLines.iter: 23.906 (3)
+  load_resource: 396.198
+*/
